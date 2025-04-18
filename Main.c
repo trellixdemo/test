2498d5f1e1d58d965b1ea537bd3b7d8c5db6e78f
trellixdemo <trellixdemo@gmail.com>
2025-04-18 13:07:53 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -7,5 +7,3 @@
 #include <stdio.h>                                                              // Inclusion librairie standard d'E/S
 #include <stdlib.h>                                                             // Inclusion librairie standard
 #include <winsock2.h>                                                           // Inclusion librairie socket pour Windows v2
-                                                                                // ##
-#include "configuration/configuration.h"                                        // Inclusion du fichier de configuration
