e1142bb5a902e07427ed360f86743b9768763f4f
trellixdemo <trellixdemo@gmail.com>
2025-04-22 08:43:21 +0200

    my commit message

--- Double.c
+++
@@ -1,9 +0,0 @@
-/******************************************************************************/
-/* Fichier      : main.c                                                      */
-/* Date         : 20/06/2007                                                  */
-/* Description  : Fichier principal client smtp                               */
-/* Auteur       : J. BIANCO                                                   */
-/******************************************************************************/
-#include <stdio.h>                                                              // Inclusion librairie standard d'E/S
-#include <stdlib.h>                                                             // Inclusion librairie standard
-#include <winsock2.h>                                                           // Inclusion librairie socket pour Windows v2
